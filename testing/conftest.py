d915bf5eda7408cec649a8b291569e4de531cdb4
yangchao <1396871335@qq.com>
2021-03-11 20:25:32 +0800

    03112025

---
+++ testing/conftest.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+'''
+@Project ：Tencent12ProTest 
+@File    ：conftest.py
+@Author  ：joseph
+@Date    ：2021-03-11 19:45 
+'''
+import pytest
+
+
+def pytest_collection_modifyitems(session, config, items: list):
+    print(items)
+    print(type(items))
+    for item in items:
+        if 'add' in item.nodeid:
+            item.add_marker(pytest.mark.add)
+        elif 'div' in item.nodeid:
+            item.add_marker(pytest.mark.div)
--- testing/test_pytest.py
+++ testing/test_pytest.py
@@ -11,20 +11,29 @@ import pytest
 
 
 class TestCalc:
-    def __init__(self):
+    def setup(self):
         self.calc = Calc()
 
-    def setUp(self) -> None:
-        pass
+    def test_cap(self):
+        result = self.calc.add(2, 2)
+        assert 4 == result
 
+    # @pytest.mark.run(order=3)
     def test_add(self):
         result = self.calc.add(1, 2)
         assert 3 == result
 
+    # @pytest.mark.run(order=2)
     def test_div(self):
         result = self.calc.div(2, 2)
         assert 1 == result
 
+    # @pytest.mark.run(order=2)
+    def test_cap_1(self):
+        result = self.calc.add(2, 2)
+        assert 4 == result
 
+
+# pytest.main(['-vs', 'test_pytest.py::TestCalc::test_div'])
 if __name__ == '__main__':
-    pytest.main(['-vs', 'test_pytest.py::TestCalc::test_div'])
+    pytest.main()
