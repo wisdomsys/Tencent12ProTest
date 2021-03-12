ffd18c201b864e53dc470dbf0decb3d5270b2bb5
yangchao <1396871335@qq.com>
2021-03-12 16:35:15 +0800

    03121635

---
+++ testing/abc_login.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+"""
+@Project ：Tencent12ProTest
+@File    ：abc_login.py
+@Author  ：joseph
+@Date    ：2021-03-12 15:01
+"""
+
+
+def test_login():
+    print('login')
+
+
+class LoginCases:
+    def case_a(self):
+        print('LoginCases::case_a')
+
+    def test_a(self):
+        print('LoginCases::test_a')
+
+    def case_b(self):
+        print('LoginCases::case_b')
