994ae73315dfd41fb01effe5687b1cdd311598b1
yangchao <1396871335@qq.com>
2021-03-12 14:32:06 +0800

    dev

--- demo.py
+++ demo.py
@@ -1,4 +1,11 @@
-from testing import *
+def foo():
+    print("starting...")
+    while True:
+        res = yield 4
+        print("res:", res)
 
 
-demo3.a
+f = foo()
+print(next(f))
+print('**' * 20)
+print(next(f))
---
+++ python/calc.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+"""
+@Project ：Tencent12ProTest
+@File    ：calc.py
+@Author  ：joseph
+@Date    ：2021-03-11 09:53
+"""
+
+
+class Calc:
+    def add(self, a: int, b: int):
+        return a + b
+
+    def minus(self, a, b):
+        return a - b
+
+    def times(self, a, b):
+        return a * b
+
+    def div(self, a, b):
+        return a / b
