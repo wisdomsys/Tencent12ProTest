39838c7721d72bb53101cc737ae465bd2c6a04ce
yangchao <1396871335@qq.com>
2021-03-12 14:15:08 +0800

    03121415

--- testing/conftest.py
+++ testing/conftest.py
@@ -10,10 +10,13 @@ import pytest
 
 
 def pytest_collection_modifyitems(session, config, items: list):
-    print(items)
-    print(type(items))
-    for item in items:
-        if 'add' in item.nodeid:
-            item.add_marker(pytest.mark.add)
-        elif 'div' in item.nodeid:
-            item.add_marker(pytest.mark.div)
+    # print(items)
+    # print(type(items))
+    # for item in items:
+    #     print('这是item', item)
+    #     if 'add' in item.nodeid:
+    #         item.add_marker(pytest.mark.add)
+    #     elif 'div' in item.nodeid:
+    #         item.add_marker(pytest.mark.div)
+
+    items.reverse()
