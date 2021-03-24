6c3133050d19afee165a35d23f0031c05704a2c2
yangchao <1396871335@qq.com>
2021-03-24 15:25:37 +0800

    03241525

---
+++ selenium_wework_main/page/add_member.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+"""
+@Date    ：2021-03-24 11:36
+"""
+from time import sleep
+
+from selenium.webdriver.common.by import By
+from selenium.webdriver.remote.webdriver import WebDriver
+
+
+class AddMember:
+    def __init__(self, driver: WebDriver):
+        self._driver = driver
+
+    def add_member(self):
+        # send_keys
+        sleep(1)
+        self._driver.find_element(By.ID, 'username').send_keys('alex')
+        self._driver.find_element(By.ID, 'memberAdd_acctid').send_keys('alex')
+        self._driver.find_element(By.ID, 'memberAdd_phone').send_keys('13788979823')
+        self._driver.find_element(By.CSS_SELECTOR, '.js_btn_save').click()
+        sleep(5)
+        return True
+
+    def get_member(self):
+        elements = self._driver.find_elements(By.CSS_SELECTOR, '.member_colRight_memberTable_td')
+        lists = [element.get_attribute('title') for element in elements]
+        print(lists)
+        return lists
--- selenium_wework_main/page/main.py
+++ selenium_wework_main/page/main.py
@@ -3,3 +3,26 @@
 """
 @Date    ：2021-03-24 11:13
 """
+import shelve
+
+from selenium.webdriver.chrome.options import Options
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+from selenium_wework_main.page.add_member import AddMember
+
+
+class Main:
+    def __init__(self):
+        # 为什么有WebDriver  语法注释的原因
+        options = Options()
+        options.debugger_address = '127.0.0.1:9222'
+        self._driver = webdriver.Chrome(options=options)
+        # self._driver = webdriver.Chrome()
+        self._driver.get('https://work.weixin.qq.com/wework_admin/frame')
+
+
+    def goto_add_member(self):
+        # 点击添加成员
+        self._driver.find_element_by_xpath('//*[@id="_hmt_click"]/div[1]/div[4]/div[2]/a[1]/div/span[2]').click()
+        return AddMember(self._driver)
